bbaaca5a54056f7795b61f8764d76075251f03a7
Caio Vinícius Menezes Gomes <c214192@g.unicamp.br>
2021-06-17 16:48:58 -0300

    Criando um pouco mais do esqueleto das cartas, classe: Traço, Efeito, Campeão e Feitiço

--- src/projeto/cartas/Carta.java
+++ src/projeto/cartas/Carta.java
@@ -3,6 +3,10 @@ package projeto.cartas;
 public abstract class Carta {
     private String nome;
     private int custo;
-
+    
+    public Carta(String nome, int custo) {
+    	this.nome = nome;    	
+    	this.custo = custo;
+    }
     /* * */
 }
--- src/projeto/cartas/Unidade.java
+++ src/projeto/cartas/Unidade.java
@@ -1,9 +1,51 @@
 package projeto.cartas;
 
+import java.util.ArrayList;
+
 public class Unidade extends Carta{
     private int vida;
     private int poder;
+    private ArrayList<Efeito> efeitos;
+    private ArrayList<Traco> tracos;
 
+    //Construtor para quando a carta tem efeito e tem traço
+    public Unidade(String nome, int custo, int vida, int poder, Efeito efeito, Traco traco) {
+    	super(nome, custo);
+    	this.vida = vida;
+    	this.poder = poder;
+    	this.efeitos = new ArrayList<>();
+    	this.tracos = new ArrayList<>();
+    	tracos.add(traco);
+    	efeitos.add(efeito);
+    }
+    
+  //Construtor para quando a carta tem efeito e não tem traço
+    public Unidade(String nome, int custo, int vida, int poder, Efeito efeito) {
+    	super(nome, custo);
+    	this.vida = vida;
+    	this.poder = poder;
+    	this.efeitos = new ArrayList<>();
+    	efeitos.add(efeito);
+    }
+    
+    //Construtor para quando a carta não tem efeito mas tem traço
+    public Unidade(String nome, int custo, int vida, int poder, Traco traco ) {
+    	super(nome, custo);
+    	this.vida = vida;
+    	this.poder = poder;
+    	this.efeitos = new ArrayList<>();
+    	this.tracos = new ArrayList<>();
+    	tracos.add(traco);
+    }  
+    
+  //Construtor para quando a carta não tem efeito e não tem traço
+    public Unidade(String nome, int custo, int vida, int poder) {
+    	super(nome, custo);
+    	this.vida = vida;
+    	this.poder = poder;
+    	this.efeitos = new ArrayList<>();
+    }
+    
     public static void batalhar(Unidade unidade1, Unidade unidade2){
         unidade1.vida -= unidade2.poder;
         unidade2.vida -= unidade1.poder;
@@ -11,5 +53,9 @@ public class Unidade extends Carta{
 
     public int getPoder() {
         return poder;
+    }
+    
+    public int getVida() {
+        return vida;
     }
 }
